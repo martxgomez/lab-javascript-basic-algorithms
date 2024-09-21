835f79d0e94727ee8fe3a6b769dc53dfe1ce2601
Marta Gómez <martagomezmartinez95@gmail.com>
2024-09-21 14:11:35 +0200

    Bonus 1

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ console.log(`The driver's name is ${hacker1}`);
 const hacker2 = "Marta";
 console.log(`The navigator's name is ${hacker2}`);
 
-
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length){
 console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
@@ -15,9 +14,8 @@ else if (hacker1.length < hacker2.length){
 
     else{
         console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`);
-        
-    }
 
+    }
 
 // Iteration 3: Loops
 let hacker1Modified = "";
@@ -50,5 +48,38 @@ if (hacker1 === hacker2){
     }
 }
 
+// Bonus 1
 
+const longText =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ullamcorper ac lorem sit amet viverra. Donec ac risus ac augue consequat tristique a vel dui. Maecenas ornare, purus in malesuada viverra, elit lacus euismod lectus, nec feugiat arcu tellus ut arcu. Maecenas vel eros ipsum. Donec at magna odio. Ut vestibulum massa non nulla tempor porttitor et ut arcu. Donec vel sapien quis nisi venenatis hendrerit ut at nibh. Quisque interdum pellentesque felis, venenatis tempor magna porta sed. Ut vitae dictum dolor. Praesent mi tellus, bibendum eget sodales eu, rutrum sit amet turpis. Maecenas aliquam porttitor est. Nullam bibendum tortor quis justo dignissim vehicula. Maecenas ullamcorper vehicula orci ornare gravida. Duis sit amet tellus a magna feugiat feugiat non vel urna. Ut auctor leo nulla, et tincidunt enim blandit sit amet. Curabitur dignissim faucibus pharetra. Curabitur ut orci ac ante dignissim lobortis. Proin non libero diam. Sed turpis purus, ultricies ac pellentesque quis, ullamcorper quis nibh. Etiam fermentum nunc quis sem blandit, sit amet venenatis lectus tempus. Donec fermentum vehicula enim eu vulputate. Proin aliquet leo ac erat interdum lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc a justo in sapien varius interdum. Vestibulum quis nisi libero. Etiam rutrum tempus ultricies. Quisque elit tellus, imperdiet sit amet bibendum vitae, rutrum quis orci. Vestibulum malesuada, sapien in tincidunt mattis, lectus mi tristique quam, ac placerat velit mi blandit urna. Sed eget aliquet dolor, ut ultricies velit. Maecenas vel nibh orci. Suspendisse potenti. Sed eget tempor ipsum, eget pretium tellus. Nulla sed lacinia elit.";
 
+let wordCount = 0;
+let actualChar = "";
+let nextChar = "";
+
+for (let i = 0; i < longText.length; i++) {
+  actualChar = longText[i];
+  nextChar = longText[i + 1];
+
+  if (
+    nextChar === " " ||
+    nextChar === "." ||
+    nextChar === "," ||
+    nextChar === "!" ||
+    nextChar === "?"
+  ) {
+    switch (actualChar) {
+      case " ":
+      case ".":
+      case ",":
+      case "!":
+      case "?":
+        continue;
+
+      default:
+        wordCount++;
+    }
+  }
+}
+
+console.log(wordCount);
